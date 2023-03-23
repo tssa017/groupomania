a0c42252f3f975ef7a2ab7613f0c88606b93d186
malonetheressa <theressadmalone@gmail.com>
2023-03-23 19:24:51 +0100

    :sparkles: implemented react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,25 @@
-import './index.scss'; // Import main stylesheet
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.scss'; // Imports main stylesheet
 import Header from './components/header/Header.js';
-// import Portal from './components/portal/Portal.js';
-// import Profile from './components/profile/Profile.js';
+import Portal from './components/portal/Portal.js';
+import Profile from './components/profile/Profile.js';
 import Status from './components/status/Status.js';
 import Settings from './components/settings/Settings.js';
 import Publication from './components/publication/Publication';
 
 function App() {
     return (
-        // <div className="app">
-        //     <Header />
-        //     <div className="app__portal">
-        //         <Portal />
-        //     </div>
-        // </div>
-        // <div className="app">
-        //     <Header />
-        //     <div className="app__profile">
-        //         <Profile />
-        //     </div>
-        // </div>
-        <div className="app">
+        <Router>
             <Header />
-            <Settings />
-            <Status />
-            <Publication />
-        </div>
+            <Routes>
+                <Route path="/" element={<Portal />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/status" element={<Status />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/publication" element={<Publication />} />
+            </Routes>
+        </Router>
     );
 }
 
--- front/src/components/portal/Portal.js
+++ front/src/components/portal/Portal.js
@@ -1,11 +1,14 @@
 import '../../index.scss';
 import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function Portal() {
     // Store and manages state of non-senistive user input
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [emailAddress, setEmailAddress] = useState('');
+    let [firstName, setFirstName] = useState('');
+    let [lastName, setLastName] = useState('');
+    let [emailAddress, setEmailAddress] = useState('');
+
+    const navigate = useNavigate();
 
     // Hook stores the state of a button so to apply styles
     const [buttonState, setButtonState] = useState({
@@ -21,133 +24,140 @@ function Portal() {
     };
 
     return (
-        <div className="portal">
-            <div className="portal__toggle">
-                <button
-                    className={`portal__toggle--signup ${
-                        buttonState.signupClicked ||
+        <div className="portal-profile__wrapper">
+            <div className="portal">
+                <div className="portal__toggle">
+                    <button
+                        className={`portal__toggle--signup ${
+                            buttonState.signupClicked ||
+                            (!buttonState.signupClicked &&
+                                !buttonState.loginClicked)
+                                ? 'clicked'
+                                : 'purple'
+                        }`}
+                        onClick={() => handleButtonClick('signup')}
+                    >
+                        Sign up
+                    </button>
+                    <button
+                        className={`portal__toggle--login ${
+                            buttonState.loginClicked ? 'clicked' : ''
+                        }`}
+                        onClick={() => handleButtonClick('login')}
+                    >
+                        Login
+                    </button>
+                </div>
+                <form className="portal__form">
+                    {(buttonState.signupClicked ||
                         (!buttonState.signupClicked &&
-                            !buttonState.loginClicked)
-                            ? 'clicked'
-                            : 'purple'
-                    }`}
-                    onClick={() => handleButtonClick('signup')}
-                >
-                    Sign up
-                </button>
-                <button
-                    className={`portal__toggle--login ${
-                        buttonState.loginClicked ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('login')}
-                >
-                    Login
-                </button>
-            </div>
-            <form className="portal__form">
+                            !buttonState.loginClicked)) && (
+                        // React fragment used to group multiple elements together without creating an extra DOM node
+                        <>
+                            <div className="portal__form-group">
+                                <input
+                                    id="firstName"
+                                    name="firstName"
+                                    type="text"
+                                    onChange={({ target }) =>
+                                        setFirstName(target.value)
+                                    }
+                                />
+                                <label htmlFor="firstName">First name</label>
+                            </div>
+                            <div className="portal__form-group">
+                                <input
+                                    id="lastName"
+                                    name="lastName"
+                                    type="text"
+                                    onChange={({ target }) =>
+                                        setLastName(target.value)
+                                    }
+                                />
+                                <label htmlFor="lastName">Last name</label>
+                            </div>
+                            <div className="portal__form-group">
+                                <input
+                                    id="emailAddress"
+                                    name="emailAddress"
+                                    type="text"
+                                    onChange={({ target }) =>
+                                        setEmailAddress(target.value)
+                                    }
+                                />
+                                <label htmlFor="emailAddress">
+                                    Email address
+                                </label>
+                            </div>
+                            <div className="portal__form-group">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="portal__form-group">
+                                <input
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    type="password"
+                                />
+                                <label htmlFor="confirmPassword">
+                                    Confirm password
+                                </label>
+                            </div>
+                        </>
+                    )}
+                    {buttonState.loginClicked && (
+                        <>
+                            <div className="portal__form-group">
+                                <input
+                                    id="emailAddress"
+                                    name="emailAddress"
+                                    type="text"
+                                    onChange={({ target }) =>
+                                        setEmailAddress(target.value)
+                                    }
+                                />
+                                <label htmlFor="emailAddress">
+                                    Email address
+                                </label>
+                            </div>
+                            <div className="portal__form-group">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </>
+                    )}
+                </form>
                 {(buttonState.signupClicked ||
                     (!buttonState.signupClicked &&
                         !buttonState.loginClicked)) && (
-                    // React fragment used to group multiple elements together without creating an extra DOM node
-                    <>
-                        <div className="portal__form-group">
-                            <input
-                                id="firstName"
-                                name="firstName"
-                                type="text"
-                                onChange={({ target }) =>
-                                    setFirstName(target.value)
-                                }
-                            />
-                            <label htmlFor="firstName">First name</label>
-                        </div>
-                        <div className="portal__form-group">
-                            <input
-                                id="lastName"
-                                name="lastName"
-                                type="text"
-                                onChange={({ target }) =>
-                                    setLastName(target.value)
-                                }
-                            />
-                            <label htmlFor="lastName">Last name</label>
-                        </div>
-                        <div className="portal__form-group">
-                            <input
-                                id="emailAddress"
-                                name="emailAddress"
-                                type="text"
-                                onChange={({ target }) =>
-                                    setEmailAddress(target.value)
-                                }
-                            />
-                            <label htmlFor="emailAddress">Email address</label>
-                        </div>
-                        <div className="portal__form-group">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                            />
-                            <label htmlFor="password">Password</label>
-                        </div>
-                        <div className="portal__form-group">
-                            <input
-                                id="confirmPassword"
-                                name="confirmPassword"
-                                type="password"
-                            />
-                            <label htmlFor="confirmPassword">
-                                Confirm password
-                            </label>
-                        </div>
-                    </>
+                    <p
+                        className={`portal__account-login ${
+                            buttonState.loginClicked ? 'clicked' : ''
+                        }`}
+                        onClick={() => handleButtonClick('login')}
+                    >
+                        <em>Already have an account?</em>
+                    </p>
                 )}
                 {buttonState.loginClicked && (
-                    <>
-                        <div className="portal__form-group">
-                            <input
-                                id="emailAddress"
-                                name="emailAddress"
-                                type="text"
-                                onChange={({ target }) =>
-                                    setEmailAddress(target.value)
-                                }
-                            />
-                            <label htmlFor="emailAddress">Email address</label>
-                        </div>
-                        <div className="portal__form-group">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                            />
-                            <label htmlFor="password">Password</label>
-                        </div>
-                    </>
+                    <p
+                        className={`portal__account-signup ${
+                            buttonState.signupClicked ? 'clicked' : ''
+                        }`}
+                        onClick={() => handleButtonClick('signup')}
+                    >
+                        <em>Don't have an account yet?</em>
+                    </p>
                 )}
-            </form>
-            {(buttonState.signupClicked ||
-                (!buttonState.signupClicked && !buttonState.loginClicked)) && (
-                <p
-                    className={`portal__account-login ${
-                        buttonState.loginClicked ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('login')}
-                >
-                    <em>Already have an account?</em>
-                </p>
-            )}
-            {buttonState.loginClicked && (
-                <p
-                    className={`portal__account-signup ${
-                        buttonState.signupClicked ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('signup')}
-                >
-                    <em>Don't have an account yet?</em>
-                </p>
-            )}
+            </div>
         </div>
     );
 }

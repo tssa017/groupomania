973bcd79c425aff4e8d80820b11123d9acd32b7e
malonetheressa <theressadmalone@gmail.com>
2023-03-23 19:25:06 +0100

    :sparkles: implemented react router

--- front/src/components/profile/Profile.js
+++ front/src/components/profile/Profile.js
@@ -2,37 +2,39 @@ import '../../index.scss';
 
 function Profile() {
     return (
-        <div className="profile">
-            <img
-                src="/images/malina.jpeg"
-                className="profile__img"
-                alt="User profile picture"
-            />
-            <button className="profile__update-btn">
-                Update profile picture
-            </button>
-            <form>
-                <div className="profile__form-group">
-                    <input id="firstName" name="firstName" type="text" />
-                    <label htmlFor="firstName">First name</label>
-                </div>
-                <div className="profile__form-group">
-                    <input id="lastName" name="lastName" type="text" />
-                    <label htmlFor="lastName">Last name</label>
-                </div>
-                <div className="profile__form-group">
-                    <input
-                        id="email"
-                        name="email"
-                        type="text"
-                        placeholder="user@example.com"
-                    />
-                    <label htmlFor="email">Email</label>
-                </div>
-            </form>
-            <button className="profile__deactivate-btn">
-                Deactivate account
-            </button>
+        <div className="portal-profile__wrapper">
+            <div className="profile">
+                <img
+                    src="/images/malina.jpeg"
+                    className="profile__img"
+                    alt="User profile picture"
+                />
+                <button className="profile__update-btn">
+                    Update profile picture
+                </button>
+                <form>
+                    <div className="profile__form-group">
+                        <input id="firstName" name="firstName" type="text" />
+                        <label htmlFor="firstName">First name</label>
+                    </div>
+                    <div className="profile__form-group">
+                        <input id="lastName" name="lastName" type="text" />
+                        <label htmlFor="lastName">Last name</label>
+                    </div>
+                    <div className="profile__form-group">
+                        <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            placeholder="user@example.com"
+                        />
+                        <label htmlFor="email">Email</label>
+                    </div>
+                </form>
+                <button className="profile__deactivate-btn">
+                    Deactivate account
+                </button>
+            </div>
         </div>
     );
 }

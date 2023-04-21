1503d16e321505da0468d3619bcd5b2e86f52c54
malonetheressa <theressadmalone@gmail.com>
2023-04-21 18:47:25 +0200

    :poop: step just before adding a field to Post

--- back/models/post.js
+++ back/models/post.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
             post: { type: DataTypes.STRING(4000) },
             postPicUrl: { type: DataTypes.STRING(512) },
             likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+            read: { type: DataTypes.BOOLEAN, defaultValue: false }, // Add read field
             usersLiked: { type: DataTypes.STRING, defaultValue: '' },
         },
         {
@@ -27,5 +28,16 @@ module.exports = (sequelize, DataTypes) => {
             modelName: 'Post',
         }
     );
+    Post.getAllPosts = async function () {
+        const posts = await Post.findAll({
+            order: [
+                // Prioritize posts that haven't been read yet
+                ['read', 'ASC'],
+                // Then sort by creation date in descending order
+                ['createdAt', 'DESC'],
+            ],
+        });
+        return posts;
+    };
     return Post;
 };
